b591cba3fc87c4eedb99936d4d8f9bf930189b75
Joyounger <942510346@qq.com>
2017-11-05 14:54:02 +0800

    also get android kernel source tree

---
+++ getkernel.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#https://github.com/aosp-mirror/kernel_common
+#https://github.com/aosp-mirror/kernel_msm
+
+type=$1
+branch=$2
+
+cat << EOF
+usage:
+    bash getaosp.sh param1 param2 param3
+    param1--the type of aosp kernel source file you want download, 'common' or 'msm'
+    param2--the branch you want to get,eg: android-4.4-o-release,android-3.18-o-release
+EOF
+
+
+
+if [ $type = "common" ]; then
+  git clone -b $branch https://github.com/aosp-mirror/kernel_common
+fi
+
+
+if [ $type = "common" ]; then
+  git clone -b $branch https://github.com/aosp-mirror/kernel_msm
+fi
+
+
